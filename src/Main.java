c9508527163d2cbac08893f46c713aef1f2f5ac7
AdeleGropplero <adele.gdt@hotmail.com>
2025-01-11 12:37:17 +0100

    inizio interfaccia utente

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import myApplication.Audio;
 import myApplication.Immagine;
+import myApplication.Video;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,11 +8,33 @@ public class Main {
     public static void main(String[] args) {
 
         Immagine img1 = new Immagine("Tramonto al parco");
+        Audio a1 = new Audio("Adele che canta", 3);
+        Video v1 = new Video("Signora i limoni", 2);
+
+
         img1.alzaLuminosità();
         img1.alzaLuminosità();
         img1.alzaLuminosità();
         img1.alzaLuminosità();
         img1.abbassaLuminosità();
         img1.show();
+
+        a1.abbassaVolume();
+        a1.abbassaVolume();
+        a1.play();
+
+        v1.alzaVolume();
+        v1.alzaVolume();
+        v1.alzaVolume();
+        v1.alzaVolume();
+        v1.alzaVolume();
+
+        v1.abbassaLuminosità();
+        v1.abbassaLuminosità();
+        v1.play();
+
+
+
+
     }
 }
